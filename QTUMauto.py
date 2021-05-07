e84ecaf0fc95b46a0d2d7ea9dcd797c392b0aecd
Harim-Jin <83169442+Harim-Jin@users.noreply.github.com>
2021-05-07 12:07:11 +0900

    Update QTUMauto.py

--- QTUMauto.py
+++ QTUMauto.py
@@ -81,5 +81,5 @@ while True:
         time.sleep(1)
     except Exception as e:
         print(e)
-        post_message(myToken,"#rbhrec", "QTUM Error" + e)
+        post_message(myToken,"#rbhrec", "QTUM Error : " + e)
         time.sleep(1)
