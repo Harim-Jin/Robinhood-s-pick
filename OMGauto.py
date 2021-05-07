b89962a4b0a260f2baba13749b3e5661f5377a80
Harim-Jin <83169442+Harim-Jin@users.noreply.github.com>
2021-05-07 12:07:24 +0900

    Update OMGauto.py

--- OMGauto.py
+++ OMGauto.py
@@ -81,5 +81,5 @@ while True:
         time.sleep(1)
     except Exception as e:
         print(e)
-        post_message(myToken,"#rbhrec", "OMG Error" + e)
+        post_message(myToken,"#rbhrec", "OMG Error : " + e)
         time.sleep(1)
