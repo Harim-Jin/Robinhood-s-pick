0d1a9e5e88494c248861ab1bfd66e4ca1c1a801f
Harim-Jin <83169442+Harim-Jin@users.noreply.github.com>
2021-05-07 12:01:29 +0900

    Update and rename LSKauto to LSKauto.py

--- LSKauto.py
+++ LSKauto.py
@@ -81,5 +81,5 @@ while True:
         time.sleep(1)
     except Exception as e:
         print(e)
-        post_message(myToken,"#rbhrec", e)
+        post_message(myToken,"#rbhrec", "LSK Error" + e)
         time.sleep(1)
