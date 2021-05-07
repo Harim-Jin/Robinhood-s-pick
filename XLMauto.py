be5e46d948bb07e06a42a46d6ca7b701eb26c128
Harim-Jin <83169442+Harim-Jin@users.noreply.github.com>
2021-05-07 12:06:55 +0900

    Update and rename XLMauto to XLMauto.py

--- XLMauto.py
+++ XLMauto.py
@@ -55,31 +55,31 @@ def get_current_price(ticker):
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 # 시작 메세지 슬랙 전송
-post_message(myToken,"#rbhrec", " 자동매매 시작!")
+post_message(myToken,"#rbhrec", "XLM 자동매매 시작!")
 
 
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-")
+        start_time = get_start_time("KRW-XLM")
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-", 0.4)
-            ma15 = get_ma15("KRW-")
-            current_price = get_current_price("KRW-")
+            target_price = get_target_price("KRW-XLM", 0.4)
+            ma15 = get_ma15("KRW-XLM")
+            current_price = get_current_price("KRW-XLM")
             if target_price < current_price and ma15 < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-", krw*0.9995)
-                    post_message(myToken,"#rbhrec", " buy : " +str(buy_result))
+                    buy_result = upbit.buy_market_order("KRW-XLM", krw*0.9995)
+                    post_message(myToken,"#rbhrec", "XLM buy : " +str(buy_result))
         else:
-            bch = get_balance("BCH")
-                if bch > 0.003:
-                sell_result = upbit.sell_market_order("KRW-BCH", bch*0.9995)
-                post_message(myToken,"#rbhrec", " sell : " +str(sell_result))
+            xlm = get_balance("XLM")
+                if xlm > 7:
+                sell_result = upbit.sell_market_order("KRW-XLM", xlm*0.9995)
+                post_message(myToken,"#rbhrec", "XLM sell : " +str(sell_result))
         time.sleep(1)
     except Exception as e:
         print(e)
-        post_message(myToken,"#rbhrec", e)
+        post_message(myToken,"#rbhrec", "XLM Error : " + e)
         time.sleep(1)
